5d2b87e0705a03e95b2ec60d72c83e07bd7e31bd
James Geater <jgeater@outlook.com>
2017-12-18 19:30:32 -0500

    initial commit
    know issue with recovery keys on some systems

--- dskcryp_Verif/Form1.Designer.cs
+++ dskcryp_Verif/Form1.Designer.cs
@@ -28,12 +28,197 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tpmRedy = new System.Windows.Forms.Label();
+            this.TPM_LockOut = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cur_BLStat = new System.Windows.Forms.Label();
+            this.SHO_BL_VER = new System.Windows.Forms.Label();
+            this.TPM_Redy_State = new System.Windows.Forms.Label();
+            this.TPM_Cur_Lock_Stat = new System.Windows.Forms.Label();
+            this.fixIt_Btn = new System.Windows.Forms.Button();
+            this.exit_Btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cur_RecKey = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tpmRedy
+            // 
+            this.tpmRedy.AutoSize = true;
+            this.tpmRedy.Location = new System.Drawing.Point(73, 67);
+            this.tpmRedy.Name = "tpmRedy";
+            this.tpmRedy.Size = new System.Drawing.Size(67, 13);
+            this.tpmRedy.TabIndex = 2;
+            this.tpmRedy.Text = "TPM Ready:";
+            // 
+            // TPM_LockOut
+            // 
+            this.TPM_LockOut.AutoSize = true;
+            this.TPM_LockOut.Location = new System.Drawing.Point(272, 67);
+            this.TPM_LockOut.Name = "TPM_LockOut";
+            this.TPM_LockOut.Size = new System.Drawing.Size(66, 13);
+            this.TPM_LockOut.TabIndex = 4;
+            this.TPM_LockOut.Text = "Locked Out:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // cur_BLStat
+            // 
+            this.cur_BLStat.AutoSize = true;
+            this.cur_BLStat.BackColor = System.Drawing.Color.Yellow;
+            this.cur_BLStat.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cur_BLStat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cur_BLStat.Location = new System.Drawing.Point(146, 32);
+            this.cur_BLStat.Name = "cur_BLStat";
+            this.cur_BLStat.Size = new System.Drawing.Size(55, 15);
+            this.cur_BLStat.TabIndex = 6;
+            this.cur_BLStat.Text = "Unknown";
+            this.cur_BLStat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SHO_BL_VER
+            // 
+            this.SHO_BL_VER.AutoSize = true;
+            this.SHO_BL_VER.BackColor = System.Drawing.Color.Yellow;
+            this.SHO_BL_VER.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SHO_BL_VER.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.SHO_BL_VER.Location = new System.Drawing.Point(344, 34);
+            this.SHO_BL_VER.Name = "SHO_BL_VER";
+            this.SHO_BL_VER.Size = new System.Drawing.Size(55, 15);
+            this.SHO_BL_VER.TabIndex = 8;
+            this.SHO_BL_VER.Text = "Unknown";
+            this.SHO_BL_VER.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TPM_Redy_State
+            // 
+            this.TPM_Redy_State.AutoSize = true;
+            this.TPM_Redy_State.BackColor = System.Drawing.Color.Yellow;
+            this.TPM_Redy_State.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TPM_Redy_State.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.TPM_Redy_State.Location = new System.Drawing.Point(146, 65);
+            this.TPM_Redy_State.Name = "TPM_Redy_State";
+            this.TPM_Redy_State.Size = new System.Drawing.Size(55, 15);
+            this.TPM_Redy_State.TabIndex = 9;
+            this.TPM_Redy_State.Text = "Unknown";
+            // 
+            // TPM_Cur_Lock_Stat
+            // 
+            this.TPM_Cur_Lock_Stat.AutoSize = true;
+            this.TPM_Cur_Lock_Stat.BackColor = System.Drawing.Color.Yellow;
+            this.TPM_Cur_Lock_Stat.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TPM_Cur_Lock_Stat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.TPM_Cur_Lock_Stat.Location = new System.Drawing.Point(344, 67);
+            this.TPM_Cur_Lock_Stat.Name = "TPM_Cur_Lock_Stat";
+            this.TPM_Cur_Lock_Stat.Size = new System.Drawing.Size(55, 15);
+            this.TPM_Cur_Lock_Stat.TabIndex = 10;
+            this.TPM_Cur_Lock_Stat.Text = "Unknown";
+            // 
+            // fixIt_Btn
+            // 
+            this.fixIt_Btn.Enabled = false;
+            this.fixIt_Btn.Location = new System.Drawing.Point(65, 314);
+            this.fixIt_Btn.Name = "fixIt_Btn";
+            this.fixIt_Btn.Size = new System.Drawing.Size(114, 56);
+            this.fixIt_Btn.TabIndex = 11;
+            this.fixIt_Btn.Text = "Fix It";
+            this.fixIt_Btn.UseVisualStyleBackColor = true;
+            this.fixIt_Btn.Click += new System.EventHandler(this.fixIt_Btn_Click);
+            // 
+            // exit_Btn
+            // 
+            this.exit_Btn.Location = new System.Drawing.Point(275, 314);
+            this.exit_Btn.Name = "exit_Btn";
+            this.exit_Btn.Size = new System.Drawing.Size(114, 56);
+            this.exit_Btn.TabIndex = 12;
+            this.exit_Btn.Text = "Exit";
+            this.exit_Btn.UseVisualStyleBackColor = true;
+            this.exit_Btn.Click += new System.EventHandler(this.exit_Btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Bitlocker Protection Status:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(249, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Bitlocker Version: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Recovery Key:";
+            // 
+            // cur_RecKey
+            // 
+            this.cur_RecKey.AutoSize = true;
+            this.cur_RecKey.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.cur_RecKey.Location = new System.Drawing.Point(91, 102);
+            this.cur_RecKey.Name = "cur_RecKey";
+            this.cur_RecKey.Size = new System.Drawing.Size(55, 15);
+            this.cur_RecKey.TabIndex = 16;
+            this.cur_RecKey.Text = "Unknown";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(453, 458);
+            this.Controls.Add(this.cur_RecKey);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.exit_Btn);
+            this.Controls.Add(this.fixIt_Btn);
+            this.Controls.Add(this.TPM_Cur_Lock_Stat);
+            this.Controls.Add(this.TPM_Redy_State);
+            this.Controls.Add(this.SHO_BL_VER);
+            this.Controls.Add(this.cur_BLStat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TPM_LockOut);
+            this.Controls.Add(this.tpmRedy);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Disk Encryption Verififcation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label tpmRedy;
+        private System.Windows.Forms.Label TPM_LockOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label cur_BLStat;
+        private System.Windows.Forms.Label SHO_BL_VER;
+        private System.Windows.Forms.Label TPM_Redy_State;
+        private System.Windows.Forms.Label TPM_Cur_Lock_Stat;
+        private System.Windows.Forms.Button fixIt_Btn;
+        private System.Windows.Forms.Button exit_Btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label cur_RecKey;
     }
 }
 
--- dskcryp_Verif/Form1.cs
+++ dskcryp_Verif/Form1.cs
@@ -4,8 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace dskcryp_Verif
 {
@@ -14,6 +17,199 @@ namespace dskcryp_Verif
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //get the current bitlocker ver and stastus
+            Process BLINFO = new Process();
+            BLINFO.StartInfo.FileName = "manage-BDE.exe";
+            BLINFO.StartInfo.Arguments = "-status c:";
+            BLINFO.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            BLINFO.StartInfo.UseShellExecute = false;
+            BLINFO.StartInfo.RedirectStandardOutput = true;
+            BLINFO.StartInfo.RedirectStandardInput = true;
+            BLINFO.StartInfo.RedirectStandardError = true;
+            BLINFO.StartInfo.CreateNoWindow = true;
+            BLINFO.Start();
+            string BLoutput = BLINFO.StandardOutput.ReadToEnd();
+            string[] result = BLoutput.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            BLINFO.WaitForExit();
+
+            //Clean up the strings
+            String cur_BLver = (result[5]);
+            cur_BLver = cur_BLver.Replace(" ", "\t");
+            String Cur_BLstat = (result[9]);
+            Cur_BLstat = Cur_BLstat.Replace(" ", "\t");
+
+            //display the Bitlocker status info in the UI
+            if (Cur_BLstat.Contains("On"))
+            {
+                cur_BLStat.BackColor = Color.LawnGreen;
+                cur_BLStat.Text = "ON";
+            }
+
+            if (Cur_BLstat.Contains("Off"))
+            {
+                cur_BLStat.BackColor = Color.Red;
+                cur_BLStat.Text = "OFF";
+                fixIt_Btn.Enabled = true;
+            }
+
+            //Display the bitlocker ver in the UI
+
+            if (cur_BLver.Contains("2.0"))
+            {
+                SHO_BL_VER.BackColor = Color.LawnGreen;
+                SHO_BL_VER.Text = "2.0";
+            }
+
+            if (Cur_BLstat.Contains("1.2"))
+            {
+                SHO_BL_VER.BackColor = Color.LawnGreen;
+                SHO_BL_VER.Text = "1.2";
+            }
+
+            //Now Gather and Set the TPM information
+            Process TPMINFO = new Process();
+            TPMINFO.StartInfo.FileName = "powershell";
+            TPMINFO.StartInfo.Arguments = "get-tpm";
+            TPMINFO.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            TPMINFO.StartInfo.UseShellExecute = false;
+            TPMINFO.StartInfo.RedirectStandardOutput = true;
+            TPMINFO.StartInfo.RedirectStandardInput = true;
+            TPMINFO.StartInfo.RedirectStandardError = true;
+            TPMINFO.StartInfo.CreateNoWindow = true;
+            TPMINFO.Start();
+            string TPMoutput = TPMINFO.StandardOutput.ReadToEnd();
+            string[] TPM_result = TPMoutput.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            TPMINFO.WaitForExit();
+
+            String TPM_READY = (TPM_result[1]);
+            //String TPM_LockedOut = (TPM_result[8]);
+
+            if (TPM_READY.Contains ("True"))
+            {
+                TPM_Redy_State.BackColor = Color.LawnGreen;
+                TPM_Redy_State.Text = "Ready";
+                //Delete reg key flag if it exists
+
+            }
+
+            if (TPM_READY.Contains("False"))
+            {
+                TPM_Redy_State.BackColor = Color.Red;
+                TPM_Redy_State.Text = "Not Ready";
+                fixIt_Btn.Enabled = true;
+                //create reg flag key if it doesnt exist
+
+            }
+
+            //get and display the TPM locked out value
+            Process TPM_LO = new Process();
+            TPM_LO.StartInfo.FileName = "powershell";
+            TPM_LO.StartInfo.Arguments = "$tpm_LO = get-tpm ; $tpm_lo.LockedOut";
+            TPM_LO.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            TPM_LO.StartInfo.UseShellExecute = false;
+            TPM_LO.StartInfo.RedirectStandardOutput = true;
+            TPM_LO.StartInfo.RedirectStandardInput = true;
+            TPM_LO.StartInfo.RedirectStandardError = true;
+            TPM_LO.StartInfo.CreateNoWindow = true;
+            TPM_LO.Start();
+            string TPMLOoutput = TPM_LO.StandardOutput.ReadToEnd();
+            string[] TPMLO_result = TPMLOoutput.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            TPMINFO.WaitForExit();
+
+
+
+            if (TPMLO_result.Contains("False"))
+            {
+                TPM_Cur_Lock_Stat.BackColor = Color.LawnGreen;
+                TPM_Cur_Lock_Stat.Text = "No";
+            }
+
+            if (TPMLO_result.Contains("True"))
+            {
+                TPM_Cur_Lock_Stat.BackColor = Color.Red;
+                TPM_Cur_Lock_Stat.Text = "YES";
+            }
+
+            
+            //get and populate the recovery key
+            Process RecINFO = new Process();
+            RecINFO.StartInfo.FileName = "manage-BDE.exe";
+            RecINFO.StartInfo.Arguments = "C: -protectors -get -type RecoveryPassword";
+            RecINFO.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            RecINFO.StartInfo.UseShellExecute = false;
+            RecINFO.StartInfo.RedirectStandardOutput = true;
+            RecINFO.StartInfo.RedirectStandardInput = true;
+            RecINFO.StartInfo.RedirectStandardError = true;
+            RecINFO.StartInfo.CreateNoWindow = true;
+            RecINFO.Start();
+            string rKeyOutput = RecINFO.StandardOutput.ReadToEnd();
+            string[] cur_Rkey = rKeyOutput.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            RecINFO.WaitForExit();
+            //clean up and display the recovery key
+            String my_Rkey = cur_Rkey[11];
+            //String my_Rkey = cur_Rkey[8];
+            my_Rkey = my_Rkey.TrimStart();
+            //need a better way, loop thorugh the results and if the length is 63 then it is a recovery key. there could be more than one
+
+            cur_RecKey.Text = (my_Rkey);
+
+
+
+        }
+
+        private void exit_Btn_Click (object sender, EventArgs e)
+        {
+            Environment.Exit(0);           
+        }
+
+        private void fixIt_Btn_Click(object sender, EventArgs e)
+        {
+            // disable Bitlocker
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.StartInfo.RedirectStandardOutput = false;
+            p.StartInfo.FileName = "Manage-bde";
+            p.StartInfo.Arguments = "-protectors -disable c:";
+            p.StartInfo.RedirectStandardInput = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.WaitForExit();
+
+            //clear the TPM
+            Process Clr_TPM = new Process();
+            Clr_TPM.StartInfo.FileName = "powershell";
+            Clr_TPM.StartInfo.Arguments = "$oTPM = gwmi -Class Win32_TPM -Namespace root\\CIMV2\\Security\\MicrosoftTpm ;$oTPM.SetPhysicalPresenceRequest(22)";
+            Clr_TPM.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Clr_TPM.StartInfo.UseShellExecute = false;
+            Clr_TPM.StartInfo.RedirectStandardOutput = true;
+            Clr_TPM.StartInfo.RedirectStandardInput = true;
+            Clr_TPM.StartInfo.RedirectStandardError = true;
+            Clr_TPM.StartInfo.CreateNoWindow = true;
+            Clr_TPM.Start();
+            Clr_TPM.WaitForExit();
+            
+            //reboot the computer
+            Process Shutdwn = new Process();
+            Shutdwn.StartInfo.UseShellExecute = false;
+            Shutdwn.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Shutdwn.StartInfo.RedirectStandardOutput = false;
+            Shutdwn.StartInfo.FileName = "cmd";
+            Shutdwn.StartInfo.Arguments = "/C shutdown -f -r -t 5";
+            Shutdwn.StartInfo.RedirectStandardInput = false;
+            Shutdwn.Start();
+            Shutdwn.WaitForExit();
+
+
+            
+
+
+
         }
     }
 }
